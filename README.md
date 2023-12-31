bc7108bb461b3cf8413b039ffc6a0f86b0d1b1c5
D_00 <69427207+d-002@users.noreply.github.com>
2023-12-31 14:28:58 +0100

    added schematics, textures, bug fixes, debug mode, updated readme, other things I don't remember

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ C++ 3D renderer, using SDL2. Work in progress, nowhere near the point where I wo
 
 Make sure to include in libraries SDL2 and SDL2_ttf (both in folders `/src/include` and `/src/bin`
 
-![](https://github.com/d-002/cpp-renderer/assets/69427207/27cef978-7294-4306-bb3a-1024ac46eb16)
+![](https://github.com/d-002/cpp-renderer/assets/69427207/576aa367-f054-4401-b811-f6babb4bea25)
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-// todo: opti background
+// TODO
+// sort cubes by distance
+// cache rotated pos
+// use arrays instead of vectors
+// bug disappear in the distance
+// matrix transformations
+/// fix bug no rotation weird z change
+/// fix render distance
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <png.h>
 #include <chrono>
 #include <thread>
 #include "vec2.cpp"
@@ -17,7 +27,8 @@ using namespace std::chrono;
 #define MOVESPEED 4
 #define ROTSPEED 2
 
-#define SKY 0xfffff0d0;
+#define TEXSIZE 16
+#define TEXSIZELOG 4
 
 bool limitFPS = false;
 
@@ -26,15 +37,15 @@ SDL_Renderer* renderer;
 
 float FOV = 90; // degrees (don't ask)
 float d = -WIDTH/2*tan(FOV*M_PI/360);
-float NEAR = 0.1; float FAR = 100;
-float renderDistance = 10;
-Vec3 camPosition = Vec3(0, 0, 5);
+float renderDistance = 20;
+Vec3 camPosition = Vec3(0, 0, 10);
 vector<float> camRotation = {0, 0};
+Uint8 sky0[3] = {130, 168, 255};
+Uint8 sky1[3] = {187, 212, 255};
+Uint32* textureMap;
 
 // warning: starts at the front (z >)
-Uint8 facesIndices[6][4] = {{0, 1, 3, 2}, {5, 4, 6, 7}, {1, 5, 7, 3}, {4, 0, 2, 6}, {4, 5, 1, 0}, {2, 3, 7, 6}};
-
-int renderMode = 0;
+Uint8 facesIndices[6][4] = {{0, 1, 3, 2}, {5, 4, 6, 7}, {4, 0, 2, 6}, {1, 5, 7, 3}, {4, 5, 1, 0}, {2, 3, 7, 6}};
 
 void renderText(string text, int x, int y, SDL_Color color, TTF_Font* font) {
     SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
@@ -49,20 +60,127 @@ void renderText(string text, int x, int y, SDL_Color color, TTF_Font* font) {
     SDL_DestroyTexture(texture);
 }
 
-void debug(float times[], SDL_Color* color, TTF_Font* font) {
-    float sum = 0;
-    for (int i = 0; i < 10; i++) {
-        sum += times[i];
-    }
-    int smoothFPS;
-    if (sum != 0) {
-        smoothFPS = 10/sum;
-    } else {
-        smoothFPS = -1;
-    }
-    renderText("FPS: " + to_string(smoothFPS), 0, 0, *color, font);
+void debug(float t, SDL_Color* color, TTF_Font* font) {
+    renderText("FPS: " + (t == 0 ? "inf" : to_string((int)(1/t))), 0, 0, *color, font);
     //renderText("Position: " + camPosition.str(), 0, 16, *color, font);
     //renderText("Rotation: " + Vec3(camRotation[0], camRotation[1]).str(), 0, 32, *color, font);
+}
+
+class Cube {
+    public:
+        Cube(Vec3 _pos, Uint8 _texId) {
+            pos = _pos;
+            texId = _texId;
+        }
+        void updateVisible(vector<Cube> cubes, int n_cubes) {
+            for (Uint8 i = 0; i < 6; i++) visible[i] = true;
+            Uint8 n = 0; // if reaches 6, all faces are hidden (no further checks)
+            for (int i = 0; i < n_cubes; i++) {
+                if (n == 6) break;
+                Vec3 p = cubes[i].pos;
+                bool ex = p.x == pos.x;
+                bool ey = p.y == pos.y;
+                bool ez = p.z == pos.z;
+                if (ex && ey) {
+                    if (p.z+1 == pos.z) { visible[0] = false; n++; }
+                    if (p.z-1 == pos.z) { visible[1] = false; n++; }
+                } if (ey && ez) {
+                    if (p.x+1 == pos.x) { visible[2] = false; n++; }
+                    if (p.x-1 == pos.x) { visible[3] = false; n++; }
+                } if (ex && ez) {
+                    if (p.y+1 == pos.y) { visible[4] = false; n++; }
+                    if (p.y-1 == pos.y) { visible[5] = false; n++; }
+                }
+            }
+        }
+        int getTexId(Uint8 face) {
+            switch (texId) {
+                case 0: return face == 4 ? 2 : face == 5 ? 0 : 1;
+                case 6: return face == 4 ? 6 : face == 5 ? 6 : 7;
+                default: return texId;
+            }
+        }
+        Vec3 pos;
+        Uint8 texId; // position in the texture
+        bool visible[6]; // don't draw hidden faces
+};
+
+Uint32* getTextureMap() {
+    /* Uint32* textureMap = (Uint32*)malloc(16*sizeof(Uint32)<<(TEXSIZELOG<<1));
+    for (int t = 0; t < 16; t++) {
+        for (int x = 0; x < TEXSIZE; x++) {
+            for (int y = 0; y < TEXSIZE; y++) {
+                int i = x + (y + (t << TEXSIZELOG) << TEXSIZELOG);
+                if ((x+y) % 2 == 0) textureMap[i] = 0xffffff;
+                else textureMap[i] = 0;
+            }
+        }
+    }
+    return textureMap; */
+
+    // thank you ChatGPT
+    FILE* file = fopen("assets/tex.png", "rb");
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info = png_create_info_struct(png);
+    png_init_io(png, file);
+    png_read_info(png, info);
+    int width = png_get_image_width(png, info);
+    int height = png_get_image_height(png, info);
+    png_read_update_info(png, info);
+    int row_bytes = png_get_rowbytes(png, info);
+    png_bytep row_pointers[height];
+
+    for (int i = 0; i < height; ++i) {
+        row_pointers[i] = new png_byte[row_bytes];
+    }
+
+    png_read_image(png, row_pointers);
+
+    // fill textureMap with png data
+    Uint32* textureMap = (Uint32*)malloc(width*height*4);
+    int w = width>>TEXSIZELOG; // number of textures per line
+    for (int Y = 0; Y < height>>TEXSIZELOG; Y++) {
+        for (int y = 0; y < TEXSIZE; y++) {
+            png_bytep row = row_pointers[y + (Y<<TEXSIZELOG)];
+            for (int X = 0; X < w; X++) {
+                for (int x = 0; x < TEXSIZE; x++) {
+                    int i = x + (y + (X + Y*w << TEXSIZELOG) << TEXSIZELOG);
+                    textureMap[i] = 0;
+                    for (int c = 0; c < 4; c++) {
+                        textureMap[i] += row[(x + (X<<TEXSIZELOG)) * 4 + c] << (c<<3);
+                    }
+                }
+            }
+        }
+    }
+
+    // clean up
+    for (int i = 0; i < height; i++) {
+        delete[] row_pointers[i];
+    }
+
+    png_destroy_read_struct(&png, &info, NULL);
+    fclose(file);
+
+    return textureMap;
+}
+
+pair<int, vector<Cube>> loadWorld(string path) {
+    vector<Cube> cubes;
+    int n_cubes = 0;
+    string line;
+    ifstream file(path);
+
+    while (getline(file, line)) {
+        istringstream iline(line);
+        vector<int> values;
+        int n;
+        while (iline >> n) values.push_back(n);
+        cubes.push_back(Cube(Vec3(values[0], values[1], values[2]), values[3]));
+        n_cubes++;
+    }
+    file.close();
+    return pair(n_cubes, cubes);
 }
 
 Vec3 rotatePoint(Vec3 point) {
@@ -72,18 +190,22 @@ Vec3 rotatePoint(Vec3 point) {
     //float x = point.x; float y = point.y; float z = point.z;
     float angle = atan2(z,x);
     if (x != 0) {
-        x /= cos(angle);
-        z = x*sin(angle-camRotation[1]);
-        x = x*cos(angle-camRotation[1]);
+        float l;
+        if (-0.01 < x && x < 0.01) l = sqrt(x*x + z*z);
+        else l = x/cos(angle);
+        z = l*sin(angle-camRotation[1]);
+        x = l*cos(angle-camRotation[1]);
     } else {
         x = z*sin(camRotation[1]);
         z = z*cos(camRotation[1]);
     }
     angle = atan2(z, y);
     if (y != 0) {
-        y /= cos(angle);
-        z = y*sin(angle-camRotation[0]);
-        y = y*cos(angle-camRotation[0]);
+        float l;
+        if (-0.01 < y && y < 0.01) l = sqrt(y*y + z*z);
+        else l = y/cos(angle);
+        z = l*sin(angle-camRotation[0]);
+        y = l*cos(angle-camRotation[0]);
     } else {
         y = z*sin(camRotation[0]);
         z = z*cos(camRotation[0]);
@@ -93,20 +215,21 @@ Vec3 rotatePoint(Vec3 point) {
     return Vec3(x, y, z);
 }
 
-vector<int16_t> project(Vec3 point) {
-    if (-point.z < NEAR || -point.z > FAR) {
+vector<int> project(Vec3 point) {
+    if (-point.z > renderDistance || point.z > 1) {
         return {NULL, NULL};
     }
-    float m = d/point.z;
-    return {(int16_t)((WIDTH>>1) + point.x*m), (int16_t)((HEIGHT>>1) - point.y*m)};
+    float m = point.z == 0 ? -d*1000 : point.z > 0 ? -d/point.z : d/point.z;
+    return {(int)((WIDTH>>1) + point.x*m), (int)((HEIGHT>>1) - point.y*m)};
 }
 
-void drawFace(vector<int16_t> points[4], Uint32* pixels) {
-    int16_t* a = &points[0][0];
-    int16_t* b = &points[1][0];
-    int16_t* c = &points[2][0];
-    int16_t* d = &points[3][0];
-    if (a[0] == d[0] && b[0] == c[0] && a[0] != b[0]) {
+void drawFace(vector<int> points[4], Uint32* pixels, Uint8 texId) {
+    int* a = &points[0][0];
+    int* b = &points[1][0];
+    int* c = &points[2][0];
+    int* d = &points[3][0];
+    int texi = texId<<(TEXSIZELOG<<1);
+    if (a[0] == d[0] && b[0] == c[0] && a[0] != b[0] && camRotation[0] == 0) {
         // straight rectangle: draw vertical lines
         float x0 = b[0] < 0 ? 0 : b[0];
         float x1 = a[0] > WIDTH ? WIDTH : a[0];
@@ -115,16 +238,16 @@ void drawFace(vector<int16_t> points[4], Uint32* pixels) {
         for (int x = x0; x < x1; x++) {
             float tx = (x-b[0]) / spanX;
 
-            float my = c[1] + (d[1]-c[1])*tx;
-            float My = b[1] + (a[1]-b[1])*tx;
-            if (my < 0) my = 0;
-            if (My > HEIGHT) My = HEIGHT;
-            float spanY = (My-my);
+            float y0 = c[1] + (d[1]-c[1])*tx;
+            float y1 = b[1] + (a[1]-b[1])*tx;
+            float my = y0 < 0 ? 0 : y0;
+            float My = y1 > WIDTH ? WIDTH : y1;
+            float spanY = (y1-y0);
 
             for (int y = my; y < My; y++) {
-                float ty = (y-my)/spanY;
+                float ty = (y-y0)/spanY;
                 if (ty < 0) ty = 0;
-                pixels[x + y*WIDTH] = (255<<24) + ((int)(255*ty)<<16) + ((int)(255 - 255*tx)<<8) + (int)(255*tx);
+                pixels[x + y*WIDTH] = textureMap[texi + (int)(tx*TEXSIZE) + ((int)(ty*TEXSIZE)<<TEXSIZELOG)];
             }
         }
     } else {
@@ -141,10 +264,10 @@ void drawFace(vector<int16_t> points[4], Uint32* pixels) {
             if (y > My) My = y;
         }
 
-        int16_t* right = &points[(topId+1)%4][0];
-        int16_t* bottom = &points[(topId+2)%4][0];
-        int16_t* left = &points[(topId+3)%4][0];
-        int16_t* top = &points[topId][0];
+        int* right = &points[(topId+1)%4][0];
+        int* bottom = &points[(topId+2)%4][0];
+        int* left = &points[(topId+3)%4][0];
+        int* top = &points[topId][0];
         if (my == bottom[1]) return;
         bool lSwap = false;
         bool rSwap = false;
@@ -223,64 +346,99 @@ void drawFace(vector<int16_t> points[4], Uint32* pixels) {
                 tyb = 1-_txb;
            }
 
-            if (x0 < 0) x0 = 0;
-            if (x1 > WIDTH) x1 = WIDTH;
+            float _x0 = x0 < 0 ? 0 : x0;
+            float _x1 = x1 > WIDTH ? WIDTH : x1;
             float spanTx = (txb-txa);
             float spanTy = (tyb-tya);
-            for (int x = x0; x < x1; x++) {
+            if (x0 == x1) continue;
+            for (int x = _x0; x < _x1; x++) {
                 float t = (x-x0)/(x1-x0);
-                if (t < 0) t = 0;
+                if (t <= 0) t = 0.01;
+                if (t > 1) t = 1;
                 float tx = txa + spanTx*t;
                 float ty = tya + spanTy*t;
-                pixels[x + y*WIDTH] = ((int)(255*ty)<<16) + ((int)(255*tx)<<8) + (int)(255-255*tx);
+                pixels[x + y*WIDTH] = textureMap[texi + (int)(tx*TEXSIZE) + ((int)(ty*TEXSIZE)<<TEXSIZELOG)];
             }
         }
     }
 }
 
 void background(Uint32* pixels) {
-    Uint8 col0[2] = {130, 168};
-    Uint8 col1[2] = {187, 212};
     Uint8 a, b;
     int bottom = (HEIGHT>>1) - sin(camRotation[0])*d;
     Uint8 height = HEIGHT>>3;
     if (bottom < 0) {
-        for (int i = 0; i < WIDTH*HEIGHT; i++) pixels[i] = (255<<16)+(col1[1]<<8)+col1[0];
+        for (int i = 0; i < WIDTH*HEIGHT; i++) pixels[i] = (sky0[2]<<16)+(sky1[1]<<8)+sky1[0];
     } else if (bottom > HEIGHT+height) {
-        for (int i = 0; i < WIDTH*HEIGHT; i++) pixels[i] = (255<<16)+(col0[1]<<8)+col0[0];
+        for (int i = 0; i < WIDTH*HEIGHT; i++) pixels[i] = (sky0[2]<<16)+(sky0[1]<<8)+sky0[0];
     } else {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 if (y < bottom) {
                     float t = (float)(y-bottom+height) / height;
                     if (t < 0) {
-                        a = col0[0];
-                        b = col0[1];
+                        a = sky0[0];
+                        b = sky0[1];
                     } else {
-                        a = col0[0] + (col1[0]-col0[0])*t;
-                        b = col0[1] + (col1[1]-col0[1])*t;
+                        a = sky0[0] + (sky1[0]-sky0[0])*t;
+                        b = sky0[1] + (sky1[1]-sky0[1])*t;
                     }
                 } else {
-                    a = col1[0];
-                    b = col1[1];
+                    a = sky1[0];
+                    b = sky1[1];
                 }
-                pixels[x + y*WIDTH] = (255<<16)+(b<<8)+a;
+                pixels[x + y*WIDTH] = 16711680+(b<<8)+a;
             }
         }
     }
 }
 
-// TODO: cache rotated pos
-void renderScene(vector<Vec3> cubePos, Uint32* pixels) {
-    cout << endl;
-    background(pixels);
-    vector<int16_t> invisible = {NULL, NULL};
+void sortCubes(int* indexes, float* dists, int n_cubes) {
+    for (int i = 0; i < n_cubes; i++) {
+        int maxi = i;
+        float max = dists[i];
+        for (int j = i+1; j < n_cubes; j++) {
+            if (dists[j] > max) {
+                maxi = j;
+                max = dists[j];
+            }
+        }
+        int _i = indexes[i];
+        indexes[i] = indexes[maxi];
+        indexes[maxi] = _i;
+        float d = dists[i];
+        dists[i] = max;
+        dists[maxi] = d;
+    }
+}
 
-    for(Vec3 pos: cubePos) {
+void renderScene(vector<Cube> cubes, int n_cubes, Uint32* pixels) {
+    background(pixels);
+    vector<int> invisible = {NULL, NULL};
+    if (!n_cubes) return;
+
+    // sort cubes: sort indexes depending on dist^2
+    int indexes[n_cubes];
+    float dists[n_cubes];
+    for (int i = 0; i < n_cubes; i++) {
+        Vec3 pos = cubes[i].pos;
+        float dx = camPosition.x-pos.x-0.5;
+        float dy = camPosition.y-pos.x-0.5;
+        float dz = camPosition.z-pos.x-0.5;
+        indexes[i] = i;
+        dists[i] = (camPosition-pos).len2();
+    }
+    sortCubes(indexes, dists, n_cubes);
+
+    for(int k = 0; k < n_cubes; k++) {
+        int i = indexes[k];
+        Cube cube = cubes[i];
+        Vec3 pos = cubes[i].pos;
         bool visible = true;
-        vector<int16_t> points[8];
+
+        vector<int> points[8];
         for (int i = 0; i < 8; i++) {
-            vector<int16_t> projected = project(rotatePoint({pos.x+i%2, pos.y+(i%4)/2, pos.z+i/4}));
+            vector<int> projected = project(rotatePoint({pos.x+i%2, pos.y+(i%4)/2, pos.z+i/4}));
             if (projected == invisible) {
                 visible = false;
                 break;
@@ -290,22 +448,22 @@ void renderScene(vector<Vec3> cubePos, Uint32* pixels) {
         if (!visible) continue;
 
         Uint8 nearest[3] = {255, 255, 255};
-        if (pos.x > camPosition.x) nearest[0] = 3;
-        else if (pos.x+1 < camPosition.x) nearest[0] = 2;
-        if (pos.y > camPosition.y) nearest[1] = 4;
-        else if (pos.y+1 < camPosition.y) nearest[1] = 5;
-        if (pos.z > camPosition.z) nearest[2] = 0;
-        else if (pos.z+1 < camPosition.z) nearest[2] = 1;
+        if (pos.x > camPosition.x && cube.visible[2]) nearest[0] = 2;
+        else if (pos.x+1 < camPosition.x && cube.visible[3]) nearest[0] = 3;
+        if (pos.y > camPosition.y && cube.visible[4]) nearest[1] = 4;
+        else if (pos.y+1 < camPosition.y && cube.visible[5]) nearest[1] = 5;
+        if (pos.z > camPosition.z && cube.visible[0]) nearest[2] = 0;
+        else if (pos.z+1 < camPosition.z && cube.visible[1]) nearest[2] = 1;
 
         for (Uint8 i : nearest) {
             if (i == 255) continue;
-            vector<int16_t> p[4] = {
+            vector<int> p[4] = {
                 points[facesIndices[i][0]],
                 points[facesIndices[i][1]],
                 points[facesIndices[i][2]],
                 points[facesIndices[i][3]]
             };
-            drawFace(p, pixels);
+            drawFace(p, pixels, cube.getTexId(i));
         }
     }
 }
@@ -316,14 +474,15 @@ int main(int argc, char *argv[]) {
     screen = SDL_CreateWindow("3D renderer", 50, 50, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font* font = TTF_OpenFont("assets/fonts/Consolas.ttf", 16);
+    TTF_Font* font = TTF_OpenFont("assets/Consolas.ttf", 16);
     SDL_Color white = {255, 255, 255};
     SDL_Color black = {0, 0, 0};
 
-    vector<Vec3> cubesPos = {Vec3(0, 0, 0)};
-    for (int x = 0; x < 20; x++) {
-        for (int y = 0; y < 20; y++) cubesPos.push_back({(float)x, (float)y, 0});
-    }
+    textureMap = getTextureMap();
+
+    pair<int, vector<Cube>> p = loadWorld("assets/worlds/tree");
+    auto [n_cubes, cubes] = p;
+    if (n_cubes > 0) for (int i = 0; i < n_cubes; i++) cubes[i].updateVisible(cubes, n_cubes);
 
     SDL_Texture* pixBuf = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
     const Uint8 *keys = SDL_GetKeyboardState(NULL);
@@ -332,7 +491,6 @@ int main(int argc, char *argv[]) {
     double prev, now;
     float timePassed = 0;
     float toWait;
-    float times[10];
     SDL_Event event;
     bool running = true;
     while (running) {
@@ -361,10 +519,10 @@ int main(int argc, char *argv[]) {
         Vec3 movement = {movementFlat.x, y, movementFlat.y};
         camPosition += movement*MOVESPEED*timePassed;
 
-        renderScene(cubesPos, pixels);
+        renderScene(cubes, n_cubes, pixels);
         SDL_UpdateTexture(pixBuf, NULL, pixels, WIDTH*sizeof(uint32_t));
         SDL_RenderCopy(renderer, pixBuf, NULL, NULL);
-        debug(times, &black, font);
+        debug(timePassed, &black, font);
 
         SDL_RenderPresent(renderer);
 
@@ -372,17 +530,11 @@ int main(int argc, char *argv[]) {
         timePassed = (now-prev) / 1000;
         prev = now;
 
-        toWait = 1/FPS - timePassed;
-        if (toWait > 0 && limitFPS) {
-            this_thread::sleep_for(milliseconds((int)(toWait*1000)));
-            timePassed = 1/FPS;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if (i == 9) {
-                times[9] = timePassed;
-            } else {
-                times[i] = times[i+1];
+        if (limitFPS) {
+            toWait = 1/FPS - timePassed;
+            if (toWait > 0) {
+                this_thread::sleep_for(milliseconds((int)(toWait*1000)));
+                timePassed = 1/FPS;
             }
         }
     }
@@ -391,5 +543,6 @@ int main(int argc, char *argv[]) {
     SDL_DestroyWindow(screen);
     SDL_Quit();
     delete[] pixels;
+    free(textureMap);
     return 0;
 }
